81f396b197a5133fa96f32aa0a890d83fb13dc72
SolR3 <bhorvat3@gmail.com>
2024-11-13 20:40:19 -0700

    more stuff to test sn20 local vali

---
+++ misc/sn20_testing/run_vali_testnet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pm2 start neurons/validator.py \
+--interpreter python3 \
+-- \
+--netuid 76 \
+--subtensor.network test \
+--wallet.name bh-wsl-coldkey \
+--wallet.hotkey bh-wsl-hotkey \
+--axon.port 10103 \
+--run_local
