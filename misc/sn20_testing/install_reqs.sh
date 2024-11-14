52b7dcfd832b358ce02e8711d2549aafc33ceebb
SolR3 <bhorvat3@gmail.com>
2024-11-13 20:56:46 -0700

    fixing install reqs script for sn20 test

--- misc/sn20_testing/install_reqs.sh
+++ misc/sn20_testing/install_reqs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m pip install -r requirements.txt
-python -m pip install -e .
-python -m pip uninstall uvloop # b/c it causes issues with threading/loops
+python3 -m pip install -r requirements.txt
+python3 -m pip install -e .
+python3 -m pip uninstall uvloop # b/c it causes issues with threading/loops
