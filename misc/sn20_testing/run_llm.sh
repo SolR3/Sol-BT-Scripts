4efbdf82d864dc3d3e8ea1e82d94c355b705313b
SolR3 <bhorvat3@gmail.com>
2024-11-17 14:13:14 -0700

    updating run_llm script for sn20 test

--- misc/sn20_testing/run_llm.sh
+++ misc/sn20_testing/run_llm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -d -p 8000:8000  --gpus all --ipc host --name mistral-instruct docker.io/vllm/vllm-openai:latest --model thesven/Mistral-7B-Instruct-v0.3-GPTQ --max-model-len 8912 --quantization gptq --dtype half
+sudo docker run -d -p 8000:8000  --gpus all --ipc host --name mistral-instruct docker.io/vllm/vllm-openai:latest --model thesven/Mistral-7B-Instruct-v0.3-GPTQ --max-model-len 8912 --quantization gptq --dtype half --gpu-memory-utilization 0.45
