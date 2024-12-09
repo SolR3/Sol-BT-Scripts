d27e5755c1ff368415685e665bb2564855be0a69
SolR3 <bhorvat3@gmail.com>
2024-12-08 19:14:56 -0700

    updated rsn52.sh

--- misc/sn52/rsn52.sh
+++ misc/sn52/rsn52.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-pm2 start \
-  auto_update.py \
-  --name auto-update-validator \
-  --interpreter $(which python3) \
-  -- \
-  --env_file .env.validator \
-  --service validator
+cd ~/.bittensor/subnets/dojo
+echo $PWD
+
+# Couldn't get conda installed for some reason
+# Using venv in the meantime
+echo "Make sure you're in the venv-sn52-dojo env"
+echo "NOTE: Not sure this is necessary when running docker directly"
+echo "TODO: Turn this into a conda env"
+sleep 5
+
+docker compose --env-file .env.validator -f docker-compose.validator.yaml down
+docker compose --env-file .env.validator -f docker-compose.validator.yaml up --build -d validator
