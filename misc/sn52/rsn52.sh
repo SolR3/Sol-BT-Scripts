0055a5749259f6bfdfc5d8b33b601221c4727719
SolR3 <bhorvat3@gmail.com>
2024-12-10 08:06:57 -0700

    updated sn52 stuff

--- misc/sn52/rsn52.sh
+++ misc/sn52/rsn52.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd ~/.bittensor/subnets/dojo
-echo $PWD
 
 # Couldn't get conda installed for some reason
 # Using venv in the meantime
@@ -10,5 +9,24 @@ echo "NOTE: Not sure this is necessary when running docker directly"
 echo "TODO: Turn this into a conda env"
 sleep 5
 
-docker compose --env-file .env.validator -f docker-compose.validator.yaml down
-docker compose --env-file .env.validator -f docker-compose.validator.yaml up --build -d validator
+# 
+# This is the way the devs say to do it
+# 
+
+# down existing validator services
+make validator-down
+make validator-pull
+
+git pull
+
+# restart validator
+make validator
+
+# run dataset collection 
+make extract-dataset
+
+# 
+# This is how the restarter script does it. But not the way the devs say to do it
+# 
+# docker compose --env-file .env.validator -f docker-compose.validator.yaml down
+# docker compose --env-file .env.validator -f docker-compose.validator.yaml up --build -d validator
