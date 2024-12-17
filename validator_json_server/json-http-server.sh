b3c0e9e987c93ba1942ed4316c6083eec33695fc
SolR3 <bhorvat3@gmail.com>
2024-12-16 22:51:29 -0700

    fixing bug

--- validator_json_server/json-http-server.sh
+++ validator_json_server/json-http-server.sh
@@ -7,7 +7,7 @@
 PORT1=8000
 # PATH_TO_BROWSE1=/home/rizzo/.bittensor/scripts/Sol-BT-Scripts/misc/subnet_data_test
 # PATH_TO_BROWSE1=/home/bhorvat/bittensor/scripts/git_projects/Sol-BT-Scripts/misc/subnet_data_test
-PATH_TO_BROWSE1=~/.validator_data/validator_data.json
+PATH_TO_BROWSE1=~/.validator_data
 
 PUBLIC_IP=$(curl -4 icanhazip.com)
 
