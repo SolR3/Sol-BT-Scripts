47074e3d4e14e297319c2b3d6ac8f5f200025306
SolR3 <bhorvat3@gmail.com>
2024-12-23 15:01:42 -0700

    updates to json http server script

--- validator_json_server/json-http-server.sh
+++ validator_json_server/json-http-server.sh
@@ -15,14 +15,14 @@ USERNAME="rizzo"
 PASSWORD="TaR3a5hJa6Kt0R5"
 
 # Open the port temporarily with UFW
-ufw allow $PORT1/tcp comment 'for serving the json file read by the huggingface page'
+sudo ufw allow $PORT1/tcp comment 'for serving the json file read by the huggingface page'
 
 # Start HTTP servers with basic authentication on the specified ports
-sudo python3 ./json_http_server $PORT1 "$PATH_TO_BROWSE1" "$USERNAME" "$PASSWORD" &
+sudo python3 $1/json_http_server $PORT1 "$PATH_TO_BROWSE1" "$USERNAME" "$PASSWORD" &
 PYTHON_PID=$!
 
 # Trap to close the port and terminate the Python process when stopping the script
-trap 'kill -9 $PYTHON_PID 2>/dev/null; ufw delete allow $PORT1/tcp; exit 0' SIGINT SIGTERM
+sudo trap 'kill -9 $PYTHON_PID 2>/dev/null; ufw delete allow $PORT1/tcp; exit 0' SIGINT SIGTERM
 
 # Print access instructions
 echo "Server running for validator json file at http://$PUBLIC_IP:$PORT1 with username: $USERNAME and password: $PASSWORD"
