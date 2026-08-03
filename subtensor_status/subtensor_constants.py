1915e7d4a5c97f051f0e1b05d17d37546e57bac9
SolR3 <bhorvat3@gmail.com>
2026-08-02 19:25:04 -0600

    subtensor_status: adding option to transfer finney block json file to jerky VM in write_finney_block

--- subtensor_status/subtensor_constants.py
+++ subtensor_status/subtensor_constants.py
@@ -1,7 +1,9 @@
 LOCAL_TIMEZONE = "MST7MDT"
+
 JSON_FILE_NAME = "subtensor_status.json"
 FINNEY_BLOCK_FILE_NAME = "finney_block.json"
 TIMESTAMP_FILE_NAME = "timestamp.json"
+
 LOCAL_SUBTENSORS = [
     "ws://subtensor-cali.rizzo.network:9944",
     "ws://subtensor-candyland.rizzo.network:9944",
@@ -12,4 +14,5 @@ LOCAL_SUBTENSORS = [
     "ws://subtensor-lite-archive.rizzo.network:9933",
     "ws://192.168.69.55:9944 (local archive)",
 ]
+
 FINNEY_INTERVAL = 1440  # In minutes (1 day)
