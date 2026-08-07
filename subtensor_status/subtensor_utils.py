520863d2dba62c678d7bf36b75157231eb7cb8f9
SolR3 <bhorvat3@gmail.com>
2026-08-06 21:49:12 -0600

    making subtensor_status scripts bittensor 11 compatible

--- subtensor_status/subtensor_utils.py
+++ subtensor_status/subtensor_utils.py
@@ -1,4 +1,14 @@
+# standard imports
 from dataclasses import dataclass
+import logging
+import sys
+import time
+
+
+@dataclass
+class FinneyBlockData:
+    block: int | None
+    time: int
 
 
 def format_time(total_time):
@@ -14,7 +24,27 @@ def format_time(total_time):
     return runtime_text
 
 
-@dataclass
-class FinneyBlockData:
-    block: int | None
-    time: int
+def _get_logger():
+
+    class BtDateFormatter(logging.Formatter):
+        def formatTime(self, record, datefmt=None):
+            created = self.converter(record.created)
+            if datefmt:
+                s = time.strftime(datefmt, created)
+            else:
+                s = time.strftime("%Y-%m-%d %H:%M:%S", created)
+            s += f".{int(record.msecs):03d}"
+            return s
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(BtDateFormatter("%(asctime)s | %(levelname)s | %(message)s"))
+
+    logger = logging.getLogger("bittensor")
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    return logger
+
+
+logger = _get_logger()
